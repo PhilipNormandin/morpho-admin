636b9a8c52d05e44cfd2725adeca00b3c6930c07
Philip Norman <phil.normandin@live.ca>
2019-11-20 17:58:49 -0500

    Update core-functions.php

--- includes/core-functions.php
+++ includes/core-functions.php
@@ -240,9 +240,9 @@ add_filter( 'contextual_help', 'morphoadmin_remove_help_tab', 999, 3 );
 
 // remove howdy
 function morphoadmin_remove_howdy( $wp_admin_bar ) {
-    $my_account = $wp_admin_bar -> get_node( 'my-account' );
-    $newtitle = $my_account -> title;
-    $wp_admin_bar -> add_node( array(
+    $my_account = $wp_admin_bar->get_node( 'my-account' );
+    $newtitle = $my_account->title;
+    $wp_admin_bar->add_node( array(
         'id' => 'my-account',
         'title' => $newtitle,
     ));
